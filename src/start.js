5272073ec0fb8accfb49508d39f7ff3155206cc2
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-23 12:38:32 -0400

    log compiled rules

--- src/start.js
+++ src/start.js
@@ -4,7 +4,7 @@ function start(rulestr) {
     ruleset = {}
     let rules = parseRule(rulestr);
     console.log('Here is the compiled version of your ruleset.')
-      //console.log(rules);
+    console.log(rules);
     console.log(' You can modify more information (color, heartbeat, perception radius, cell acceleration, cell velocity) with that format');
     console.log(' You can load that file with start(your_compiled_ruleset)')
     reset(rules);
