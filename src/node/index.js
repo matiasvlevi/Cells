7d7def80ab5a05d67e1bae2527ae7ef2d206e364
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-07-23 19:18:38 -0400

    Rtxt as separate file

--- src/node/index.js
+++ src/node/index.js
@@ -1,24 +1,5 @@
-const fs = require('fs');
-
-class Rtxt {
-
-}
-Rtxt.stringToJSON = require('../parseRule');
-Rtxt.parseToLine = function(path) {
-  let data = fs.readFileSync(path, 'utf-8');
-  let lines = data.split('\n');
-  let readLines = '';
-  for (let i = 0; i < lines.length; i++) {
-    if (lines[i][0] !== '#') {
-      readLines += lines[i].split('\r')[0];
-    }
-  }
-  return readLines;
-}
-Rtxt.fileToJSON = function(path) {
-  return Rtxt.stringToJSON(Rtxt.parseToLine(file))
-}
-
+const Rtxt = require('./rtxt');
+console.log(Rtxt)
 
 let name = process.argv[2] || 'set_a';
 let file = ('./ruletext/' + name + '.rtxt');
